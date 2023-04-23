5a809f9385a650b3312c36f8f330d8bd1f3e30b0
hyunzsu <kkm5169@naver.com>
2023-04-23 14:48:40 +0900

    [Add] 공통 컴포넌트 세팅 - 버튼 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import RouteTest from './components/RouteTest';
-
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
+
+// COMPONENTS
+import MyButton from './components/MyButton';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
         <h2>App.js</h2>
+
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼 클릭')}
+          type={'positive'}
+        />
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼 클릭')}
+          type={'negative'}
+        />
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼 클릭')}
+        />
+
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/new' element={<New />} />
           <Route path='/edit' element={<Edit />} />
           <Route path='/diary/:id' element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
