8e445b3e505dacbc1dc23279e01bb3ffd5c1756b
hyunzsu <kkm5169@naver.com>
2023-04-23 15:34:35 +0900

    [Add] 공통 컴포넌트 세팅 - 버튼 컴포넌트

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import Diary from './pages/Diary';
 
 // COMPONENTS
 import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
+        <MyHeader
+          headText={'App'}
+          leftChild={
+            <MyButton text={'왼쪽 버튼'} onClick={() => alert('왼쪽 클릭')} />
+          }
+          rightChild={
+            <MyButton text={'오른쪽 버튼'} onClick={() => alert('오른쪽 클릭')} />
+          }
+        />
         <h2>App.js</h2>
 
         <MyButton
@@ -25,10 +35,7 @@ function App() {
           onClick={() => alert('버튼 클릭')}
           type={'negative'}
         />
-        <MyButton
-          text={'버튼'}
-          onClick={() => alert('버튼 클릭')}
-        />
+        <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} />
 
         <Routes>
           <Route path='/' element={<Home />} />
